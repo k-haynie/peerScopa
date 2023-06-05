e4d583cd83b404c831c7cab4c4ef9e0afafb0f9b
khayni <76216676+khayni@users.noreply.github.com>
2023-06-04 21:58:38 -0400

    relabled n organized code

--- p2p.js
+++ p2p.js
@@ -1,76 +1,86 @@
-// create a new peer identity
-// has to be here for scope purposes
-
 // to run offline: 
 // pull up Admin CMD and use the command: peerjs --port 9000 --key peerjs --path /myapp
 // var peer = new Peer({host: "localhost", port: 9000, path: "/myapp"});
+// instead of linking unpkg in the .html file, link locally to the 'node_modules/peerjs/dist/peerjs.min.js' file
 
-// variables for both host and player
+// general variables necessary for both host and player
 var peer = new Peer();
 let body = document.getElementById("body");
 
-// detect when a browser is closed
-window.onunload = window.oneforunload = (e) => {
-	if (!!peer && !peer.destroyed) {
-		peer.destroy();
-	}
+// callback handling - https://stackoverflow.com/questions/26958404/peerjs-peer-not-receiving-data?rq=3
+// variables for host
+let host = false; 			// activated if a host is initiated 
+let openRoom = true; 		// boolean flag to manage connections
+let peers = []; 			// list of connected peer IDs
+let gameStats = {}; 		// map of RTCconnections to game statistics
+let field = []; 			// list of integers of active playing cards
+let cards = []; 			// list of the deck of cards
+
+// variables for player
+let connected = false; 		// either a flag or the host's ID, if connected
+let turn = false; 			// boolean flag to allow/disallow turn button
+let turnCompleted = true; 	// used by the host to regulate player order
+let hand = []; 				// list of integers of the hand for each player
+
+// data structure used by gameStats to map connections to statistics
+let player = {
+	points: 0,
+	tricks: []
 }
 
+// detect when a browser is closed and destroy the peer connection
+window.onunload = window.oneforunload = (e) => { (!!peer && !peer.destroyed) ? peer.destroy() : null}
 
-// upon initialization, publish the ID via HTML
-peer.on('open', function(id){
-	console.log("Peer initialized");
-});
+// you're a lifesaver, tdxius https://stackoverflow.com/questions/22125865/how-to-wait-until-a-predicate-condition-becomes-true-in-javascript
+const waitUntil = (condition, checkInterval=100) => {
+    return new Promise(resolve => {
+        let interval = setInterval(() => {
+            if (!condition()) return;
+            clearInterval(interval);
+            resolve();
+        }, checkInterval)
+    })
+};
 
-// when a peer reaches out, publish the connecting ID
+// when a peer reaches out, allow the connection if the game is not full
 peer.on('connection', (conn) => {
-	
-	// Publish the peer connection to the host
 	let connectedPeers = document.getElementById("connectedPeers");
-	console.log(peers.length);
 	
 	if (peers.length === 4) {
 		openRoom = false;
-		conn.send("Room full");
-		return;
-	}
+	} 
 	else if (host && connectedPeers && !connectedPeers.innerHTML.includes(conn.peer)){
 		connectedPeers.innerHTML += `<li class="players">${conn.peer}</li>`;
 		peers.push(conn);
 	}
-	else if (!host && conn.data != "Room full") {	// Publish the peer connection to the player
+	else if (!host && conn.data != "Room full") {
 		connected = conn;
 		body.innerHTML = `
-		<h4 class="headertext">You have successfully been connected!</h4>
-		<p>Awaiting the host to start the game.</p>
-		`;
+			<h4 class="headertext">You have successfully been connected!</h4>
+			<p>Awaiting the host to start the game.</p>`;
 	}
 	
-	conn.on("open", () => {	// set up a response upon an open connection
+	conn.on("open", () => {
 		console.log("primary peer.on callback");
-		console.log("connection opened");
 
+		if (!openRoom && !peers.includes(peer)) {conn.send("Room full"); return};
 		
-		conn.on("data", (data) => {	// set up a response upon receiving data
-			console.log("Data handled by peer.on function");
+		// manage connections - other data is handled in connFire
+		conn.on("data", (data) => {
+			// reject data if trying to connect to a closed-off host
 			if (!host && (connected == false)) {console.log("Not connected, returning"); return};
 
-			// establish a two-way connection when a remote reaches out
-			if (host && data === "Connection received") {
-				console.log("Firing connection back")
+			// host fires a connection back to a curious peer
+			if (host && data === "Connection received" && openRoom) {
 				connFire(conn.peer);
 			}
-			console.log(data);
 		});
 		
-		// handle a disconnection
+		// upon a disconnection, update the host's screen and `peers` list
 		conn.on("close", () => {	
-			console.log("closed from primary peer.con");
-
 			if (host) {
 				let players = document.getElementsByClassName("players");
 				
-				// remove the appropriate label from the host screen
 				for (let i = players.length - 1; i > -1; --i) {
 					if (players[i].innerText == conn.peer) {
 						players[i].remove();
@@ -83,10 +93,10 @@ peer.on('connection', (conn) => {
 	});	
 });	
 
-// When the connection button is hit, send out a connection 
-//    request via PeerJS
+// connection via player's button (or host response)
+// herein lies core data parsing to manage player-side game logic
 function connFire(id=null) {
-	if (host && peers.length > 4) {return}
+	if (host && peers.length > 4) {return}		// prevent host from connecting if the game room is full
 	
 	let conn = peer.connect(id == null ? document.getElementById("peerID").value : id)
 
@@ -94,31 +104,12 @@ function connFire(id=null) {
 		console.log("connFire callback");
 		conn.send("Connection received");
 
-		// handle a disconnection
-		conn.on("close", () => {	
-			console.log("closed from connFire");
-
-			if (!host) {
-				connected = false;
-				body.innerHTML = `
-				<h4 class="headertext">The host has disconnected! Try again.</h4>
-				<input id="peerID" type="text"></input>
-				<button onclick="connFire()">Connect</button>`;	
-			}
-		});
-
 		conn.on("data", (data) => {
+
 			console.log("Data handled by connFire " + data);
 
-			if (!host && !connected) {console.log("Not connected, returning"); return};
-
-			// establish a two-way connection when a remote reaches out
-			if (host && data === "Connection received") {
-				console.log("Firing connection back")
-				connFire(conn.peer);
-			}
 			// prevent a connection if the room is full
-			else if (data === "Room full") {
+			if (data === "Room full") {
 				conn.close();
 				console.log("Room is full");
 				body.innerHTML = `
@@ -141,7 +132,7 @@ function connFire(id=null) {
 			// if it is a player's turn
 			else if (data == "CHOOSE") {
 				turn = true;
-				alert("Your turn!");
+				document.getElementById("turnBtn").disabled = false;
 			} 
 			// if a turn has been completed
 			else if (data.includes("TURN")) {
@@ -162,20 +153,20 @@ function connFire(id=null) {
 			else {
 				console.log(data);
 			};
-		})
+		});
+
+		// handle a disconnection
+		conn.on("close", () => {	
+			if (!host) {
+				connected = false;
+				body.innerHTML = `
+				<h4 class="headertext">The host has disconnected! Try again.</h4>
+				<input id="peerID" type="text"></input>
+				<button onclick="connFire()">Connect</button>`;	
+			}
+		});
 	});
 };
-
-// callback handling
-// https://stackoverflow.com/questions/26958404/peerjs-peer-not-receiving-data?rq=3
-// host.js
-// variables for host
-let host = false;
-let openRoom = true;
-let peers = [];
-let gameStats = {};
-let field = [];
-let cards = [];
 
 // initialize the host display
 async function startHost() {
@@ -184,26 +175,28 @@ async function startHost() {
 	<h1>You are the Game Host!</h1>
 	<h4 id="peerDisplay"></h4>
 	<ul id="connectedPeers"></ul>
-	<button onclick="startGame()">Start Game</button>
-	`;
-	// need to make asynchronous 
-	await publishID();
+	<button onclick="startGame()">Start Game</button>`;
+	// dubious asynchronicity 
+	await waitUntil(() => (peer.id != null));
+	publishID();
 };
 
-// publish the host ID to the interface
+// initialize the player display
+function joinGame() {
+	body.innerHTML = `
+	<h3 class="headertext">Enter the following information below to join a game:</h3>
+	<input id="peerID" type="text"></input>
+    <button onclick="connFire()">Connect</button>`;
+};
+
+// publish the host ID to the interface -- need to update for response delays
 async function publishID() {
 	let visualTag = document.getElementById("peerDisplay");
 	if (visualTag) {visualTag.innerText += "You are " + peer.id;};
 	peers.push(peer.id);
 };
 
-const gameOver = () => {
-	let totalPoints = [];
-	Object.keys(gameStats).forEach(player => {totalPoints.push(gameStats[player].points)});
-	return totalPoints.some(score => score >= 14);
-}
-
-// start a game with valid players
+// primary game-running logic -- need to update to tally score
 async function startGame() {
 	if (1 >= peers.length || peers.length > 4) {
 		alert("You need 2-4 players to start a game.");
@@ -234,15 +227,13 @@ async function startGame() {
 				dealCards();
 				broadcast("GO");
 				printHands();
-				console.log("REDALT", hand, hand.length);
 
 				while (hand.length > 0) {
-					console.log("HOST HAND", hand, hand.length);
 					turnCompleted = false;
 					
 					if (activePlayerIndex === 0) {
 						turn = true;
-						alert("Your turn!");
+						document.getElementById("turnBtn").disabled = false;
 					} 
 					else {
 						peers[activePlayerIndex].send("CHOOSE");
@@ -251,8 +242,6 @@ async function startGame() {
 					await waitUntil(() => turnCompleted);
 					activePlayerIndex += 1;
 					activePlayerIndex %= peers.length;
-
-					console.log("player change, hand length: " + hand.length + hand);
 				};
 			};
 			
@@ -270,117 +259,43 @@ async function startGame() {
 			// update Stats here			
 		}
 	}
-}
+};
 
-const waitUntil = (condition, checkInterval=100) => {
-    return new Promise(resolve => {
-        let interval = setInterval(() => {
-            if (!condition()) return;
-            clearInterval(interval);
-            resolve();
-        }, checkInterval)
-    })
-}
+// deal 3 cards to each player (data side)
+function dealCards() {
+	for (let i = 0; i < 3; ++i) {
+		peers.forEach(peer => {
+			typeof(peer) === "string" ? hand.push(cards.pop()) : peer.send(`DEAL${cards.pop()}`);
+		})			
+	}
+};
 
-// populate the 4-card playing field
+// print out the hands dealt (graphical side)
+function printHands() {
+	let visualCards = document.getElementById("cardHand");
+	hand.forEach(card => visualCards.innerHTML += `<option value=${card}>${card}</option>`);
+};
+
+// populate the 4-card playing field (data side)
 function populateField() {
 	for (let i = 0; i < 4; ++i) {
 		field.push(cards.pop());
 	}
-}
+};
 
-// deal 3 cards to each player
-function dealCards() {
-	for (let i = 0; i < 3; ++i) {
-		peers.forEach(peer => {
-			console.log("dealing")
-			typeof(peer) === "string" ? hand.push(cards.pop()) : peer.send(`DEAL${cards.pop()}`);
-		})			
-	}
-}
-
-// send a message to all players
-function broadcast(msg, callback=null) {
-	console.log("Broadcasting " + msg);
-	peers.forEach(peer => {
-		if (typeof(peer) != "string") {
-			console.log(peer, typeof(peer));
-			peer.send(msg);
-		}
-		else if (callback) {
-			callback(msg);
-		}
-	})
-}
-
+// print the playing field (graphical side)
 function publishField(data) {
 	body.innerHTML = `
 	<ul id="cardField"></ul>
 	<select id="cardHand"></select>
-	<button onclick="playerTurn()">Go</button>`;
+	<button id="turnBtn" onclick="playerTurn()" disabled="true">Go</button>`;
 	let cardField = document.getElementById("cardField");
 	let actualData = data.slice(5, -1);
 	console.log(actualData);
 	actualData.split(".").forEach(card => cardField.innerHTML += `<li><input type="checkbox">${parseInt(card)}</input></li>`);
-}
+};
 
-// player.js
-// variables for player
-let connected = false;
-let turn = false;
-let turnCompleted = true;
-let hand = [];
-
-let player = {
-	points: 0,
-	tricks: []
-}
-
-function printHands() {
-	let visualCards = document.getElementById("cardHand");
-	hand.forEach(card => visualCards.innerHTML += `<option value=${card}>${card}</option>`);
-}
-
-function joinGame() {
-	body.innerHTML = `
-	<h3 class="headertext">Enter the following information below to join a game:</h3>
-	<input id="peerID" type="text"></input>
-    <button onclick="connFire()">Connect</button>
-	`;
-}
-
-function playerTurn(conn) {
-	if (turn) {
-		let cardField = document.getElementById("cardField");
-		let fieldSelectedTotal = [];
-		let handSelected = document.getElementById("cardHand");
-		
-		for (let i = 0; i < cardField.children.length; i++){
-			if (cardField.children[i].children[0].checked)
-				fieldSelectedTotal.push(cardField.children[i].childNodes[1].data);
-		};		
-		
-
-		if (fieldSelectedTotal.length === 0 ||
-			(fieldSelectedTotal.length != 0 && parseInt(fieldSelectedTotal.reduce((sum, int) => parseInt(sum) + parseInt(int)%10))%10 === parseInt(handSelected.value%10))) {
-			
-			let transferData = `TURN${handSelected.value}/${fieldSelectedTotal.reduce((sum, card) => sum + card + ".", "")}`;
-			console.log("hand pre-splice " + hand);
-			hand.splice(hand.indexOf(parseInt(handSelected.value)), 1);
-			console.log("hand post-splice " + hand);
-			handSelected.options[handSelected.selectedIndex].remove();
-			(host ? handleTurn(transferData, peer) : connected.send(transferData));
-
-			turn = false;
-		} 
-		else {
-			alert("Please make a valid choice.");
-		}		
-	}
-	
-}
-
-// update the field when cards are removed
+// update the playing field when cards are removed (data & graphical sides)
 function updateField(data) {
 	let cardField = document.getElementById("cardField").getElementsByTagName("li");
 
@@ -394,38 +309,86 @@ function updateField(data) {
 			};
 		};			
 	});
-}
+};
 
-// handle a player's turn -- only called by the host
+// send a message to all players (callback applies to host only)
+function broadcast(msg, callback=null) {
+	console.log("Broadcasting " + msg);
+	peers.forEach(peer => {
+		if (typeof(peer) != "string") {
+			console.log(peer, typeof(peer));
+			peer.send(msg);
+		}
+		else if (callback) {
+			callback(msg);
+		};
+	});
+};
+
+// update to actually work
+const gameOver = () => {
+	let totalPoints = [];
+	Object.keys(gameStats).forEach(player => {totalPoints.push(gameStats[player].points)});
+	return totalPoints.some(score => score >= 14);
+};
+
+// called when a player hits go; parses input and passes on to host
+function playerTurn(conn) {
+	if (turn) {
+		let cardField = document.getElementById("cardField");
+		let fieldSelectedTotal = [];
+		let handSelected = document.getElementById("cardHand");
+		
+		for (let i = 0; i < cardField.children.length; i++){
+			if (cardField.children[i].children[0].checked)
+				fieldSelectedTotal.push(cardField.children[i].childNodes[1].data);
+		};		
+		
+
+		if (fieldSelectedTotal.length === 0 ||
+			(fieldSelectedTotal.length != 0 && 
+				parseInt(fieldSelectedTotal.reduce((sum, int) => parseInt(sum) + parseInt(int)%10))%10 === parseInt(handSelected.value%10))) {
+			
+			let transferData = `TURN${handSelected.value}/${fieldSelectedTotal.reduce((sum, card) => sum + card + ".", "")}`;
+			hand.splice(hand.indexOf(parseInt(handSelected.value)), 1);
+			handSelected.options[handSelected.selectedIndex].remove();
+
+			(host ? handleTurn(transferData, peer) : connected.send(transferData));
+			document.getElementById("turnBtn").disabled = true;
+			turn = false;
+		} 
+		else {
+			alert("Please make a valid choice.");
+		};
+	};
+};
+
+// host-side turn processing
 function handleTurn(turnDataRaw, conn=null) {
 	let turnData = turnDataRaw.slice(4).split("/");
 	console.log("turnData " + turnData);
 
-	// in case a card was not merely getting laid down
+	// a normal trick turn
 	if (turnData.length > 1 && turnData[1].split(".").length > 1) {
-		let nonHand = turnData[1].split(".")
-		
-		console.log(turnData);
-		// update the field, locally and remotely
 		updateField(`UPDATE${turnData[1]}`);
 		broadcast(`UPDATE${turnData[1]}`);
 		
 		// update player points
 		(host ? gameStats[peer].tricks.push(turnData) : gameStats[conn].tricks.push(turnData));
 		
-		// in case there is a scopa
+		// in case of scopa, add score & send alerts
 		if (field.length === 0) {
 			(conn ? gameStats[conn].points += 1 : gameStats[peer].points += 1);
 			alert("There has been a Scopa!");
 			broadcast("SCOPA");
-		}			
+		};	
 	}
-	// discard to the field, update local and remotes
+
+	// a discard turn (no trick)
 	else {
-		console.log(parseInt(turnData[0]), turnData[0]);
 		field.push(parseInt(turnData[0]));
 		cardField.innerHTML += `<li><input type="checkbox">${parseInt(turnData[0])}</input></li>`;
 		broadcast(`DISCARD${turnData[0]}`);
-	}
+	};
 	turnCompleted = true;
 }
